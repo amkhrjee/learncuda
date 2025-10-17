ec441f606062a90f3bdcb00b8c553aea7d5961a9
amkhrjee <aniruddha.mukherji@outlook.com>
2025-10-17 14:56:32 +0530

    fix: update matrix addition kernel for correct indexing and dimensions

--- src/matrixAdd.cu
+++ src/matrixAdd.cu
@@ -1,12 +1,13 @@
 #include <iostream>
 
-#define N 10
+#define N 256
 
 __global__ void MatAdd(int A[N][N], int B[N][N], int C[N][N])
 {
-    int i = threadIdx.x;
-    int j = threadIdx.y;
-    C[i][j] = A[i][j] + B[i][j];
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < N && j < N)
+        C[i][j] = A[i][j] + B[i][j];
 }
 
 int main(void)
@@ -27,21 +28,15 @@ int main(void)
     cudaMalloc(&d_B, sizeof(int) * N * N);
     cudaMalloc(&d_C, sizeof(int) * N * N);
 
-    cudaMemcpy(d_A, A, N * N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, N * N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, A, N * N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, N * N * sizeof(int), cudaMemcpyHostToDevice);
 
-    dim3 threadsPerBlock(N, N);
-    int numBlocks = 1;
+    dim3 threadsPerBlock(16, 16);
+    dim3 numBlocks(N / threadsPerBlock.x, N / threadsPerBlock.y);
     //  We're running one 2D thread block of dim N x N
     MatAdd<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C);
     cudaMemcpy(C, d_C, N * N * sizeof(float), cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            printf("%d\t", C[i][j]);
-        printf("\n");
-    }
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
