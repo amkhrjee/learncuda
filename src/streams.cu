51600ff05189e86bbb9489f6998ed07856d18850
amkhrjee <aniruddha.mukherji@outlook.com>
2025-10-20 16:35:01 +0530

    add: two streams

--- src/streams.cu
+++ src/streams.cu
@@ -30,19 +30,7 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    cudaEvent_t start, stop;
-    float elapsedTime;
-
-    HANDLE_ERROR(cudaEventCreate(&start));
-    HANDLE_ERROR(cudaEventCreate(&stop));
-    HANDLE_ERROR(cudaEventRecord(start, 0));
-
     int *host_a, *host_b, *host_c;
-    int *dev_a, *dev_b, *dev_c;
-
-    HANDLE_ERROR(cudaMalloc(&dev_a, N * sizeof(int)));
-    HANDLE_ERROR(cudaMalloc(&dev_b, N * sizeof(int)));
-    HANDLE_ERROR(cudaMalloc(&dev_c, N * sizeof(int)));
 
     // allocate page-locked memory used to stream
     HANDLE_ERROR(cudaHostAlloc(&host_a, FULL_DATA_SIZE * sizeof(int), cudaHostAllocDefault));
@@ -56,23 +44,48 @@ int main(void)
     }
 
     // initialiazing the stream
-    cudaStream_t stream;
-    HANDLE_ERROR(cudaStreamCreate(&stream));
+    cudaStream_t stream0, stream1;
+    HANDLE_ERROR(cudaStreamCreate(&stream0));
+    HANDLE_ERROR(cudaStreamCreate(&stream1));
+
+    int *dev_a0, *dev_b0, *dev_c0; // For stream0
+    int *dev_a1, *dev_b1, *dev_c1; // For stream1
+
+    HANDLE_ERROR(cudaMalloc(&dev_a0, N * sizeof(int)));
+    HANDLE_ERROR(cudaMalloc(&dev_b0, N * sizeof(int)));
+    HANDLE_ERROR(cudaMalloc(&dev_c0, N * sizeof(int)));
+
+    HANDLE_ERROR(cudaMalloc(&dev_a1, N * sizeof(int)));
+    HANDLE_ERROR(cudaMalloc(&dev_b1, N * sizeof(int)));
+    HANDLE_ERROR(cudaMalloc(&dev_c1, N * sizeof(int)));
+
+    cudaEvent_t start, stop;
+    float elapsedTime;
+
+    HANDLE_ERROR(cudaEventCreate(&start));
+    HANDLE_ERROR(cudaEventCreate(&stop));
+    HANDLE_ERROR(cudaEventRecord(start, 0));
 
     // Launch kernels in chunks of N
-    for (int i = 0; i < FULL_DATA_SIZE; i += N)
+    for (int i = 0; i < FULL_DATA_SIZE; i += N * 2)
     {
-        HANDLE_ERROR(cudaMemcpyAsync(dev_a, host_a + i, N * sizeof(int), cudaMemcpyHostToDevice, stream));
-        HANDLE_ERROR(cudaMemcpyAsync(dev_b, host_b + i, N * sizeof(int), cudaMemcpyHostToDevice, stream));
+        // For stream0
+        HANDLE_ERROR(cudaMemcpyAsync(dev_a0, host_a + i, N * sizeof(int), cudaMemcpyHostToDevice, stream0));
+        HANDLE_ERROR(cudaMemcpyAsync(dev_a1, host_a + i + N, N * sizeof(int), cudaMemcpyHostToDevice, stream1));
+        HANDLE_ERROR(cudaMemcpyAsync(dev_b0, host_b + i, N * sizeof(int), cudaMemcpyHostToDevice, stream0));
+        HANDLE_ERROR(cudaMemcpyAsync(dev_b1, host_b + i + N, N * sizeof(int), cudaMemcpyHostToDevice, stream1));
+        // For stream1
 
-        kernel<<<N / 256, 256, 0, stream>>>(dev_a, dev_b, dev_c);
+        kernel<<<N / 256, 256, 0, stream0>>>(dev_a0, dev_b0, dev_c0);
+        kernel<<<N / 256, 256, 0, stream1>>>(dev_a1, dev_b1, dev_c1);
 
         // Copy the data from device to locked memory
-        HANDLE_ERROR(cudaMemcpyAsync(host_c + i, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost, stream));
-
-        // Make sure all ops on the stream are finished
-        HANDLE_ERROR(cudaStreamSynchronize(stream));
+        HANDLE_ERROR(cudaMemcpyAsync(host_c + i, dev_c0, N * sizeof(int), cudaMemcpyDeviceToHost, stream0));
+        HANDLE_ERROR(cudaMemcpyAsync(host_c + i + N, dev_c1, N * sizeof(int), cudaMemcpyDeviceToHost, stream1));
     }
+
+    HANDLE_ERROR(cudaStreamSynchronize(stream0));
+    HANDLE_ERROR(cudaStreamSynchronize(stream1));
 
     HANDLE_ERROR(cudaEventRecord(stop, 0));
     HANDLE_ERROR(cudaEventSynchronize(stop));
@@ -84,10 +97,15 @@ int main(void)
     HANDLE_ERROR(cudaFreeHost(host_a));
     HANDLE_ERROR(cudaFreeHost(host_b));
     HANDLE_ERROR(cudaFreeHost(host_c));
-    HANDLE_ERROR(cudaFree(dev_a));
-    HANDLE_ERROR(cudaFree(dev_b));
+    HANDLE_ERROR(cudaFree(dev_a0));
+    HANDLE_ERROR(cudaFree(dev_b0));
+    HANDLE_ERROR(cudaFree(dev_c0));
+    HANDLE_ERROR(cudaFree(dev_a1));
+    HANDLE_ERROR(cudaFree(dev_b1));
+    HANDLE_ERROR(cudaFree(dev_c1));
 
-    HANDLE_ERROR(cudaStreamDestroy(stream));
+    HANDLE_ERROR(cudaStreamDestroy(stream0));
+    HANDLE_ERROR(cudaStreamDestroy(stream1));
 
     return 0;
 }
